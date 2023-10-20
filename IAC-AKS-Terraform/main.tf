7e3938f8557c59a456389586f8822b33725624e1
jagadish88t <107781655+jagadish88t@users.noreply.github.com>
2023-10-20 15:07:18 -0400

    added tags for resource group

--- IAC-AKS-Terraform/main.tf
+++ IAC-AKS-Terraform/main.tf
@@ -2,10 +2,12 @@ module "createrg" {
   source = "./resourcegroup"
   resource_group_name = var.resource_group_name
   resource_group_location = var.resource_group_location
+  resource_group_tags = var.resource_group_tags
 }
 
 module "createstorageaccount" {
   source = "./storageaccount"
   resource_group_name = module.createrg.resourcegroupname
   resource_group_location = module.createrg.resourcegrouplocation
+  depends_on = [ module.createrg ]
 }
--- IAC-AKS-Terraform/resourcegroup/variables.tf
+++ IAC-AKS-Terraform/resourcegroup/variables.tf
@@ -6,3 +6,6 @@ variable "resource_group_location" {
   type        = string
   description = "Resource group location for AKS"
 }
+variable "resource_group_tags" {
+  type = map(string)
+}
